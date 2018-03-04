bcebd3618d8031437f154f12d3f753576e387222
Mike Stankiewicz <stankiem89@gmail.com>
2018-03-04 10:02:05 -0700

    added files and folders with code

---
+++ notes.js
@@ -0,0 +1,8 @@
+// Aesthetics
+// 1.) A form with 10 questions and each question has 5 bubbles to choose from to the right
+// 2.) New question pops up on the page, overwriting the one prior (document.write)?
+// 3.) Questions are appended over one another like a homework sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS
+
+// Form idea = seems most difficult, but maybe think it through. Feel like it would be easier to store the information by choosing a number that way. Would likely look the best as well.
+// Overwrite idea = ...kind of ok, a little goofy. Could put a new picture above each new question, making it have a chance to be funnier.
+// Append idea = boring. Probably the easiest when it comes down to it, though.
